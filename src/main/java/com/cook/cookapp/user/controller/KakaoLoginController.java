178c59e00ca82074b7ba058964447700ba9be898
hsmygit <myhskim79@gmail.com>
2025-03-13 03:04:27 +0900

    :sparkles: Feat [#7] 카카오 로그인 구현.

---
+++ src/main/java/com/cook/cookapp/user/controller/KakaoLoginController.java
@@ -0,0 +1,48 @@
+package com.cook.cookapp.user.controller;
+
+import com.cook.cookapp.apiPayload.ApiResponse;
+import com.cook.cookapp.common.security.JwtTokenProvider;
+import com.cook.cookapp.user.dto.res.KakaoUserInfoResponseDto;
+import com.cook.cookapp.user.dto.res.UserDtoRes;
+import com.cook.cookapp.user.entity.User;
+import com.cook.cookapp.user.repository.UserRepository;
+import com.cook.cookapp.user.service.KakaoService;
+import com.cook.cookapp.user.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("")
+public class KakaoLoginController {
+
+    private final KakaoService kakaoService;
+    private final UserService userService;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final UserRepository userRepository;
+
+    @GetMapping("/callback")
+    public ApiResponse<UserDtoRes.UserLoginRes> callback(@RequestParam("code") String code, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String accessToken = kakaoService.getAccessTokenFromKakao(code);
+
+        KakaoUserInfoResponseDto userInfo = kakaoService.getUserInfo(accessToken);
+
+        //회원가입, 로그인 동시진행
+        return ApiResponse.onSuccess(userService.kakaoLogin(request,response, userService.kakaoSignup(userInfo)));
+    }
+
+    @PostMapping("whoami")
+    public Long whoami() {
+        Long userIdFromToken = jwtTokenProvider.getUserIdFromToken();
+
+        User user = userRepository.findById(userIdFromToken).orElseThrow(() -> new IllegalArgumentException("유저를 찾지 못했습니다."));
+        //회원가입, 로그인 동시진행
+        return user.getId();
+    }
+}
--- src/main/java/com/cook/cookapp/user/controller/UserController.java
+++ src/main/java/com/cook/cookapp/user/controller/UserController.java
@@ -1,6 +1,15 @@
 package com.cook.cookapp.user.controller;
 
+import com.cook.cookapp.apiPayload.ApiResponse;
+import com.cook.cookapp.common.security.JwtTokenProvider;
+import com.cook.cookapp.user.dto.req.UserDtoReq;
+import com.cook.cookapp.user.dto.res.UserDtoRes;
+import com.cook.cookapp.user.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
 public class UserController {
+    
+    private final UserService userService;
+    private final JwtTokenProvider jwtTokenProvider;
+    @PostMapping("/login")
+    public ApiResponse<UserDtoRes.UserLoginRes> login(@RequestBody UserDtoReq.LoginReq loginDto, HttpServletRequest request, HttpServletResponse response) {
+
+        return ApiResponse.onSuccess(userService.login(request,response,loginDto));
+    }
 }
