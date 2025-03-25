0c6071e6ff94b7e6a91bf7229b375ac59372b289
asdasd571 <kng955626@gmail.com>
2025-03-25 15:50:53 +0900

    :bug: Fix [#76] PostController에서 패키지 import 오류 수정

--- src/main/java/com/cook/cookapp/post/converter/PostConverter.java
+++ src/main/java/com/cook/cookapp/post/converter/PostConverter.java
@@ -3,7 +3,6 @@ package com.cook.cookapp.post.converter;
 import com.cook.cookapp.post.dto.req.PostDtoReq;
 import com.cook.cookapp.post.dto.res.PostResDto;
 import com.cook.cookapp.post.entity.Post;
-import com.cook.cookapp.recipe.dto.res.RecipeResDto;
 import com.cook.cookapp.recipe.entity.Recipe;
 import com.cook.cookapp.user.entity.User;
 import org.springframework.stereotype.Component;
