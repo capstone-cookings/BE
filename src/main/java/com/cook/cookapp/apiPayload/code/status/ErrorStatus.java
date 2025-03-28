25091fde3c50a2205197c354df5421350b157114
hsmygit <myhskim79@gmail.com>
2025-03-29 05:15:00 +0900

    :sparkles: Feat [#79] JWT 토큰 관리 Redis 전환 및 TTL 설정 적용(로컬)

--- src/main/java/com/cook/cookapp/apiPayload/code/status/ErrorStatus.java
+++ src/main/java/com/cook/cookapp/apiPayload/code/status/ErrorStatus.java
@@ -18,6 +18,8 @@ public enum ErrorStatus implements BaseErrorCode {
     JWT_EMPTY(HttpStatus.UNAUTHORIZED, "JWT4003", "JWT 토큰을 넣어주세요."),
     JWT_EXPIRED(HttpStatus.UNAUTHORIZED, "JWT4004", "만료된 토큰입니다."),
     JWT_REFRESHTOKEN_NOT_MATCHED(HttpStatus.UNAUTHORIZED, "JWT4005", "RefreshToken이 일치하지 않습니다."),
+    JWT_REFRESH_TOKEN_NOT_FOUND(HttpStatus.UNAUTHORIZED, "JWT4031", "리프레시 토큰이 존재하지 않거나 만료되었습니다."),
+    JWT_REFRESH_TOKEN_INVALID(HttpStatus.UNAUTHORIZED, "JWT4032", "유효하지 않은 리프레시 토큰입니다."),
 
     // 가장 일반적인 응답
     _INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON500", "서버 에러, 관리자에게 문의 바랍니다."),
